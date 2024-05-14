d9cd34e02c0b8323f628394275faa5b00181b5bd
Huy Hung <huyhungsp2003@gmail.com>
2024-05-14 15:28:40 +0700

    feat: Create Login

--- src/validation/product.js
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-export const productValid = Joi.object({
-    name: Joi.string().required().min(6),
-    price: Joi.number().required().min(0),
-    description: Joi.string(),image: Joi.string(),
-    categoryId: Joi.string().required()
-})

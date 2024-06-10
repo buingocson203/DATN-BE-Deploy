de3e47a45c141c7fa3b3c36166c886900fc64b44
Huy Hung <huyhungsp2003@gmail.com>
2024-06-10 22:25:51 +0700

    Done: Name A-Z, DESC, ASC, Search by product name

--- src/controllers/infoProduct.js
+++ src/controllers/infoProduct.js
@@ -8,25 +8,31 @@ const { ObjectId } = mongoose.Types;
 
 export const getInfoProductDetails = async (req, res) => {
   try {
-    const { size, category, minPrice, maxPrice, sort } = req.query; // Lấy kích thước, danh mục, khoảng giá và tham số sắp xếp từ query params
+    const { size, category, minPrice, maxPrice, sort, name } = req.query; // Lấy kích thước, danh mục, khoảng giá và tham số sắp xếp từ query params
 
     console.log("Size query:", size);
     console.log("Category query:", category);
     console.log("Min Price query:", minPrice);
     console.log("Max Price query:", maxPrice);
     console.log("Sort query:", sort);
+    console.log("Name query:", name);
 
     let sortOption = {};
     if (sort === "desc") {
       sortOption = { price: -1 }; // Giảm dần (DESC)
     } else if (sort === "asc") {
-      sortOption = { price: +1 }; // Tăng dần (ASC)
+      sortOption = { price: 1 }; // Tăng dần (ASC)
+    } else if (sort === "name") {
+      sortOption = { nameProduct: 1 }; // Sắp xếp tên từ A-Z
     }
 
     // Điều kiện lọc cho sản phẩm
     const productFilter = {};
     if (category) {
       productFilter.categoryId = new ObjectId(category);
+    }
+    if (name) {
+      productFilter.name = new RegExp(name, 'i'); // Tìm kiếm tên sản phẩm không phân biệt chữ hoa chữ thường
     }
 
     const products = await Product.find(productFilter)
@@ -112,11 +118,17 @@ export const getInfoProductDetails = async (req, res) => {
     const productDetails = await Promise.all(productDetailsPromises);
 
     // Sắp xếp dữ liệu sản phẩm dựa trên sortOption
-    if (Object.keys(sortOption).length !== 0) {
+    if (sortOption.price) {
       productDetails.sort((a, b) => {
         if (!a || !a.productDetails || a.productDetails.length === 0) return -1;
         if (!b || !b.productDetails || b.productDetails.length === 0) return 1;
-        return a.productDetails[0].price - b.productDetails[0].price;
+        return sortOption.price * (a.productDetails[0].price - b.productDetails[0].price);
+      });
+    } else if (sortOption.nameProduct) {
+      productDetails.sort((a, b) => {
+        if (!a || !a.nameProduct) return -1;
+        if (!b || !b.nameProduct) return 1;
+        return a.nameProduct.localeCompare(b.nameProduct);
       });
     }
 

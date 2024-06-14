35ef390cdc819f1a8fa3b93186764dd55aac5fce
Huy Hung <huyhungsp2003@gmail.com>
2024-06-14 23:34:47 +0700

    update: Upload Image, get 4 info Product detail

--- src/routes/image.js
+++ src/routes/image.js
@@ -1,13 +1,12 @@
 import express from "express";
 
 import { checkPermission } from "../middlewares/checkPermission.js";
-import { createImageProduct, deleteImages, getAllImages, getAllImagesByProductId } from "../controllers/image.js";
+import { createImageProduct, deleteImages, getAllImages, getAllImagesByProductId, updateImages } from "../controllers/image.js";
 
 const routerImage = express.Router();
 routerImage.get("/:productId", checkPermission, getAllImagesByProductId);
 routerImage.get("/", checkPermission, getAllImages);
 routerImage.post("/", checkPermission, createImageProduct);
 routerImage.delete("/deleteImages", checkPermission, deleteImages);
-// routerProductDetail.put("/:id", checkPermission, updateProductDetail);
-// routerProductDetail.delete("/:id", checkPermission, deleteProductDetail);
+routerImage.put("/update", checkPermission, updateImages);
 export default routerImage;

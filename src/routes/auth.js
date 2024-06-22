3ff83b4f35498a4509a4e514350d9c97920bbd6f
Huy Hung <huyhungsp2003@gmail.com>
2024-06-23 05:34:10 +0700

    feat: done account

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,12 @@
 import express from "express"
 import {Router} from "express"
-import { signIn, signUp } from "../controllers/auth.js"
+import { blockUser, signIn, signUp, updateUser, updateUserAdmin,  } from "../controllers/auth.js"
+import { checkPermission } from "../middlewares/checkPermission.js"
 
 const routerAuth = Router()
 routerAuth.post('/signup', signUp)
 routerAuth.post('/signin', signIn)
+routerAuth.put('/users/:userId', checkPermission, updateUser);
+routerAuth.put('/user-admin/:userId', checkPermission, updateUserAdmin);
+routerAuth.put('/user-block/:userId', checkPermission, blockUser);
 export default routerAuth
--- src/validation/account.js
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-export const accountValid = Joi.object({
-  userId: Joi.string().required(),
-  name: Joi.string().required(),
-  phone: Joi.string().required(),
-  gender: Joi.string().valid("male", "female", "other").required(),
-  role: Joi.string().valid("admin", "member").optional(), // optional for updates
-});

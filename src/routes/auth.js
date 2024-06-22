36654fd74377a59fa8732f3ee78e060478e719c6
Huy Hung <huyhungsp2003@gmail.com>
2024-06-23 06:09:52 +0700

    update: get all, get detail account by hunglh tamtv

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,6 @@
 import express from "express"
 import {Router} from "express"
-import { blockUser, signIn, signUp, updateUser, updateUserAdmin,  } from "../controllers/auth.js"
+import { blockUser, getAllAccount, getDetailAccount, signIn, signUp, updateUser, updateUserAdmin,  } from "../controllers/auth.js"
 import { checkPermission } from "../middlewares/checkPermission.js"
 
 const routerAuth = Router()
@@ -9,4 +9,6 @@ routerAuth.post('/signin', signIn)
 routerAuth.put('/users/:userId', checkPermission, updateUser);
 routerAuth.put('/user-admin/:userId', checkPermission, updateUserAdmin);
 routerAuth.put('/user-block/:userId', checkPermission, blockUser);
+routerAuth.get('/get-all-account', checkPermission, getAllAccount);
+routerAuth.get('/get-detail-account/:userId', checkPermission, getDetailAccount);
 export default routerAuth

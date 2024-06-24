30493352ecad2e0863e3478771fb3e6274aadfb9
Huy Hung <huyhungsp2003@gmail.com>
2024-06-25 01:38:25 +0700

    update: API 25/06

--- src/routes/review.js
+++ src/routes/review.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { checkPermission } from "../middlewares/checkPermission.js";
-import { createReview, getProductReviews, getReviewDetail } from "../controllers/review.js";
+import { createReview, getAllReview, getProductReviews, getReviewDetail } from "../controllers/review.js";
 
 const routerReview = express.Router();
 routerReview.post("/reviews", checkPermission, createReview);
 routerReview.get("/reviews/:productId/list-review", checkPermission, getProductReviews);
 routerReview.get("/reviews/:reviewId", checkPermission, getReviewDetail);
+routerReview.get("/reviews", checkPermission, getAllReview);
 export default routerReview;

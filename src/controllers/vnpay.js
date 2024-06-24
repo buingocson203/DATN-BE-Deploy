693dcef78a1ee4bd8f56f74be24e92891d4170f7
Trinh Vinh Tam <tamtv28092003@gmail.com>
2024-06-25 01:27:17 +0700

    feat: fix vnpay

--- src/controllers/vnpay.js
+++ src/controllers/vnpay.js
@@ -23,7 +23,12 @@ function sortObject(obj) {
 
 const checkoutVnpay = {
   payment: async (req, res) => {
+    // data body send request
+    let date = new Date();
+    let createDate = moment(date).format("YYYYMMDDHHmmss");
+
     try {
+      const { total_price, user_id } = req.body;
       const secretKey = process.env.VNP_HASHSECRET;
       let vnpUrl = process.env.VNP_URL;
       const ip =
@@ -31,19 +36,31 @@ const checkoutVnpay = {
         req.connection.remoteAddress ||
         req.socket.remoteAddress ||
         req.connection.socket.remoteAddress;
-      const amount = req.body.total_price;
+
+      const amount = total_price;
+      // kiểm tra nếu giá tiền dưới 5k thì k đc
+      if (amount < 5000) {
+        return res.status(500).json({
+          message: "Minimum amount is 5000 VND  ",
+        });
+      }
+
       let vnp_Params = {};
       vnp_Params["vnp_Version"] = "2.1.0";
       vnp_Params["vnp_Command"] = "pay";
       vnp_Params["vnp_TmnCode"] = process.env.VNP_TMNCODE;
       vnp_Params["vnp_Amount"] = amount * 100;
       vnp_Params["vnp_BankCode"] = "NCB";
-      vnp_Params["vnp_CreateDate"] = moment(new Date()).format("YYYYMMDDHHmmss");
+      vnp_Params["vnp_CreateDate"] = createDate;
       vnp_Params["vnp_CurrCode"] = "VND";
       vnp_Params["vnp_IpAddr"] = ip;
       vnp_Params["vnp_Locale"] = "vn";
       vnp_Params["vnp_OrderInfo"] = "Thanh_toan_don_hang";
-      vnp_Params["vnp_ReturnUrl"] = `http://localhost:5173/payment?expire=${moment(new Date())
+      vnp_Params[
+        "vnp_ReturnUrl"
+      ] = `http://localhost:5173/payment?userId=${user_id}&expire=${moment(
+        new Date()
+      )
         .add(15, "minute")
         .toDate()
         .getTime()}`;
@@ -53,13 +70,75 @@ const checkoutVnpay = {
       vnp_Params = sortObject(vnp_Params);
       const signData = querystring.stringify(vnp_Params, { encode: false });
       const hmac = crypto.createHmac("sha512", secretKey);
-      const signed = hmac.update(signData).digest("hex");
+      const signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
       vnp_Params["vnp_SecureHash"] = signed;
       vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: false });
-      
-      return { url: vnpUrl, vnp_TxnRef }; // Trả về thông tin thanh toán
+
+      return res.status(200).json({ url: vnpUrl, vnp_TxnRef }); // Trả về thông tin thanh toán
     } catch (error) {
-      throw new Error("Error server");
+      res.status(200).json({ message: "Error server" });
+    }
+  },
+
+  checkPaymentForVNPay2(req, res, next) {
+    var vnp_Params = req.query;
+
+    var secureHash = vnp_Params["vnp_SecureHash"];
+    console.log(secureHash);
+
+    delete vnp_Params["vnp_SecureHash"];
+    delete vnp_Params["vnp_SecureHashType"];
+
+    vnp_Params = sortObject(vnp_Params);
+
+    var tmnCode = process.env.VNP_TMNCODE;
+    var secretKey = process.env.VNP_HASHSECRET;
+
+    var signData = querystring.stringify(vnp_Params, { encode: false });
+
+    var hmac = crypto.createHmac("sha512", secretKey);
+    var signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
+    console.log(signed);
+    var orderId = vnp_Params["vnp_TxnRef"];
+    var rspCode = vnp_Params["vnp_ResponseCode"];
+    if (secureHash === signed) {
+      res
+        .status(200)
+        .json({ RspCode: rspCode, Message: "success", orderId, rspCode });
+    } else {
+      res.status(200).json({ RspCode: rspCode, Message: "Fail checksum" });
+    }
+  },
+  checkPaymentForVNPay(req, res, next) {
+    console.log(req.query);
+    let vnp_Params = req.query;
+    const secretKey = process.env.VNP_HASHSECRET;
+
+    const secureHash = vnp_Params["vnp_SecureHash"];
+    console.log(secureHash, "secureHash");
+
+    delete vnp_Params["vnp_SecureHash"];
+    delete vnp_Params["vnp_SecureHashType"];
+
+    vnp_Params = sortObject(vnp_Params);
+
+    vnp_Params = sortObject(vnp_Params);
+    const signData = querystring.stringify(vnp_Params, { encode: false });
+    console.log(secretKey, "dfdsfa", signData);
+    const hmac = crypto.createHmac("sha512", secretKey);
+    const signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
+
+    console.log("signed", signed);
+    var orderId = vnp_Params["vnp_TxnRef"];
+    var rspCode = vnp_Params["vnp_ResponseCode"];
+    if (secureHash === signed) {
+      return res
+        .status(200)
+        .json({ RspCode: "00", Message: "success", orderId, rspCode });
+    } else {
+      return res
+        .status(200)
+        .json({ orderId, rspCode, RspCode: "97", Message: "Fail checksum" });
     }
   },
 };
--- src/routes/order.js
+++ src/routes/order.js
@@ -1,28 +1,25 @@
 import express from "express";
 
-import { createOrder, getAllOrders, getOrderDetail, updateOrder } from "../controllers/order.js";
+import {
+  createOrder,
+  getAllOrders,
+  getOrderDetail,
+  updateOrder,
+} from "../controllers/order.js";
 import checkoutVnpay from "../controllers/vnpay.js";
 import { checkPermission } from "../middlewares/checkPermission.js";
 
 const orderRouter = express.Router();
-orderRouter.post("/create-order-vnpay", async (req, res) => {
-    try {
-      // Call vnpay payment function
-      const { url, vnp_TxnRef } = await checkoutVnpay.payment(req, res);
-      // Add vnp_TxnRef to the request body as codeOrders
-      req.body.codeOrders = vnp_TxnRef;
-      req.body.paymentMethod = "vnpay"; // Ensure the payment type is set to vnpay
-      // Call createOrder function
-      const createOrderResponse = await createOrder(req, res);
-      // Send the payment URL as the response
-      res.json({ paymentUrl: url, order: createOrderResponse });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
+orderRouter.post("/create-order-vnpay", checkoutVnpay.payment);
+
+orderRouter.post(
+  "/check-paid-vnpay",
+
+  checkoutVnpay.checkPaymentForVNPay2
+);
 orderRouter.post("/create-order", checkPermission, createOrder);
 orderRouter.get("/orders", checkPermission, getAllOrders);
-orderRouter.get("/orders/:orderId", checkPermission,  getOrderDetail);
+orderRouter.get("/orders/:orderId", checkPermission, getOrderDetail);
 orderRouter.patch("/update-order/:orderId", checkPermission, updateOrder);
 
-export default orderRouter; 
+export default orderRouter;
